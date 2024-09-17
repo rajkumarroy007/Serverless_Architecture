e4e3e1d55286dd82cd23140793d939b3ad3d298e
rajkumarroy007 <rajkumarroy007@yahoo.com>
2024-09-17 23:36:11 +0530

    EC2-Stop-Start-Tag-Lambda.py

--- Lambda_and_Boto3.py
+++ Lambda_and_Boto3.py
@@ -1,54 +1,28 @@
 import boto3
-
-# Initialize EC2 client
-ec2 = boto3.client('ec2')
+import json
 
 def lambda_handler(event, context):
-    # List to hold instances to start and stop
-    instances_to_start = []
-    instances_to_stop = []
+    ec2_connection = boto3.resource(service_name="ec2", region_name="us-west-2")
 
-    try:
-        # Describe instances with the tag Auto-Start=True
-        start_response = ec2.describe_instances(
-            Filters=[
-                {'Name': 'tag:Rajkumar-Auto-Start', 'Values': ['True']}
-            ]
-        )
+    # Stop EC2 instances with tag 'Rajkumar-Auto-Stop'
+    stop_filter = {"Name": "tag:Name", "Values": ["Rajkumar-Auto-Stop"]}
+    for instance in ec2_connection.instances.filter(Filters=[stop_filter]):
+        print(f"Stopping instance: {instance.id}")
+        try:
+            instance.stop()
+        except Exception as e:
+            print(f"Error stopping instance {instance.id}: {str(e)}")
 
-        # Collect instance IDs for instances that should be started
-        for reservation in start_response['Reservations']:
-            for instance in reservation['Instances']:
-                if instance['State']['Name'] == 'stopped':
-                    instances_to_start.append(instance['InstanceId'])
-
-        # Describe instances with the tag Auto-Stop=True
-        stop_response = ec2.describe_instances(
-            Filters=[
-                {'Name': 'tag:Rajkumar-Auto-Stop', 'Values': ['True']}
-            ]
-        )
-
-        # Collect instance IDs for instances that should be stopped
-        for reservation in stop_response['Reservations']:
-            for instance in reservation['Instances']:
-                if instance['State']['Name'] == 'running':
-                    instances_to_stop.append(instance['InstanceId'])
-
-        # Start instances
-        if instances_to_start:
-            ec2.start_instances(InstanceIds=instances_to_start)
-            print(f"Started instances: {instances_to_start}")
-
-        # Stop instances
-        if instances_to_stop:
-            ec2.stop_instances(InstanceIds=instances_to_stop)
-            print(f"Stopped instances: {instances_to_stop}")
-
-    except Exception as e:
-        print(f"Error managing EC2 instances: {str(e)}")
+    # Start EC2 instances with tag 'Rajkumar-Auto-Start'
+    start_filter = {"Name": "tag:Name", "Values": ["Rajkumar-Auto-Start"]}
+    for instance in ec2_connection.instances.filter(Filters=[start_filter]):
+        print(f"Starting instance: {instance.id}")
+        try:
+            instance.start()
+        except Exception as e:
+            print(f"Error starting instance {instance.id}: {str(e)}")
 
     return {
         'statusCode': 200,
-        'body': 'Lambda function executed successfully'
+        'body': 'EC2 instances managed based on tags.'
     }
